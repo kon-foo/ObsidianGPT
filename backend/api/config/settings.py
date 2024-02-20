3f8ad2639189c4d830165a72c5fc9771cd9b68fc
kon-foo <25391223+kon-foo@users.noreply.github.com>
2024-02-20 19:07:05 +0100

    renamed to vault

--- backend/api/config/settings.py
+++ backend/api/config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     api_key: str
-    vault_dir: str
+    vault_dir: str = "vault"
     ignore_dirs: list[str] = [".obsidian", ".trash", "templates"]
 
 
